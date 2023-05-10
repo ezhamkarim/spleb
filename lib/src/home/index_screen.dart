6b3ef4de2b063273e3b79cdf837787d82a4302f6
ezhamkarim <ezhamezzuddin@gmail.com>
2023-05-10 11:20:05 +0800

    add drawer

--- lib/src/home/index_screen.dart
+++ lib/src/home/index_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:spleb/src/root/screens.dart';
 import 'package:spleb/src/style/style.dart';
 
@@ -11,37 +10,67 @@ class IndexScreen extends StatefulWidget {
 }
 
 class _IndexScreenState extends State<IndexScreen> {
-  List<Widget> screens = [const HomeScreen(), const ProjectScreen(), const TetapanScreen()];
+  // List<Widget> screens = [const HomeScreen(), const ProjectScreen(), const TetapanScreen()];
 
-  var items = [
-    const BottomNavigationBarItem(icon: Icon(FontAwesomeIcons.house), label: 'Utama'),
-    const BottomNavigationBarItem(icon: Icon(FontAwesomeIcons.diagramProject), label: 'Project'),
-    const BottomNavigationBarItem(icon: Icon(FontAwesomeIcons.gear), label: 'Tetapan')
-  ];
-  var header = ['Utama', 'Project', 'Tetapan'];
-  int currentIndex = 0;
+  // var items = [
+  //   const BottomNavigationBarItem(icon: Icon(FontAwesomeIcons.house), label: 'Utama'),
+  //   const BottomNavigationBarItem(icon: Icon(FontAwesomeIcons.diagramProject), label: 'Project'),
+  //   const BottomNavigationBarItem(icon: Icon(FontAwesomeIcons.gear), label: 'Tetapan')
+  // ];
+  // var header = ['Utama', 'Project', 'Tetapan'];
+  // int currentIndex = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: CustomColor.primary,
-          title: Text(header.elementAt(currentIndex)),
-        ),
-        body: screens.elementAt(currentIndex),
-        bottomNavigationBar: BottomNavigationBar(
-            elevation: 4,
-            currentIndex: currentIndex,
-            selectedIconTheme: IconThemeData(color: CustomColor.primary.shade700),
-            unselectedIconTheme: const IconThemeData(color: Colors.grey),
-            showSelectedLabels: true,
-            showUnselectedLabels: true,
-            selectedItemColor: CustomColor.primary.shade700,
-            unselectedItemColor: Colors.grey,
-            onTap: (i) {
-              setState(() {
-                currentIndex = i;
-              });
+      appBar: AppBar(
+        backgroundColor: CustomColor.primary,
+        // title: Text(header.elementAt(currentIndex)),
+        title: const Text('Utama'),
+      ),
+      // body: screens.elementAt(currentIndex),
+      body: const HomeScreen(),
+      drawer: Drawer(
+        child: ListView(children: [
+          Container(
+            padding: const EdgeInsets.all(16),
+            height: 200,
+            color: CustomColor.primary,
+            child: Column(mainAxisAlignment: MainAxisAlignment.end, crossAxisAlignment: CrossAxisAlignment.start, children: [
+              ClipOval(
+                child: Image.asset(
+                  'assets/images/ic_launcher_spleb.png',
+                  height: 80,
+                ),
+              )
+            ]),
+          ),
+          ListTile(
+            title: const Text('Utama'),
+            trailing: const Icon(
+              Icons.home,
+              color: CustomColor.primary,
+            ),
+            onTap: () {
+              Navigator.of(context).pushNamed('/index');
             },
-            items: items));
+          ),
+        ]),
+      ),
+      // bottomNavigationBar: BottomNavigationBar(
+      //     elevation: 4,
+      //     currentIndex: currentIndex,
+      //     selectedIconTheme: IconThemeData(color: CustomColor.primary.shade700),
+      //     unselectedIconTheme: const IconThemeData(color: Colors.grey),
+      //     showSelectedLabels: true,
+      //     showUnselectedLabels: true,
+      //     selectedItemColor: CustomColor.primary.shade700,
+      //     unselectedItemColor: Colors.grey,
+      //     onTap: (i) {
+      //       setState(() {
+      //         currentIndex = i;
+      //       });
+      //     },
+      //     items: items)
+    );
   }
 }
